87e6618dd7f5da30740fb98d1446dc966ab36544
Kuldeep1176 <kuldeepkkulkarni86@gmail.com>
2026-01-02 13:35:08 +0530

    Pushing Lab002_keywords

---
+++ src/ex_01_Java_Basics/Lab003_Variables.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+import java.lang.classfile.Signature;
+
+public class Lab003_Variables {
+    public static void main(String[] args)
+    {
+        byte age_kuldeep = 39;
+        System.out.println(age_kuldeep);
+        age_kuldeep = 40;
+        System.out.println(age_kuldeep);
+        System.out.printf("The age of Kuldeep is %d",age_kuldeep);
+    }
+}
